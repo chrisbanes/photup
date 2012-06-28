84ddfdbf993926f75cef834a56e6b0e9713b13ad
Chris Banes <chris@senab.co.uk>
2012-06-28 20:09:43 +0100

    Initial commit

---
+++ client/src/uk/co/senab/footo/PhotoActivity.java
@@ -0,0 +1,13 @@
+package uk.co.senab.footo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PhotoActivity extends Activity {
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+}
