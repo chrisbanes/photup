40f77909e01541dc09104dc50cdc859e5d112a54
Chris Banes <chris@senab.co.uk>
2012-07-06 17:16:42 +0100

    New Upload Activity

--- client/src/uk/co/senab/photup/UploadActivity.java
+++ client/src/uk/co/senab/photup/UploadActivity.java
@@ -2,38 +2,52 @@ package uk.co.senab.photup;
 
 import uk.co.senab.photup.service.PhotoUploadService;
 import uk.co.senab.photup.service.PhotoUploadService.ServiceBinder;
-import android.app.Activity;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
 
-public class UploadActivity extends Activity implements ServiceConnection {
-	
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
+import com.lightbox.android.photoprocessing.R;
+
+public class UploadActivity extends SherlockActivity implements ServiceConnection {
+
 	private ServiceBinder<PhotoUploadService> mBinder;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		bindService(new Intent(this, PhotoUploadService.class), this, Context.BIND_AUTO_CREATE);
-		
-		Button button = new Button(this);
-		button.setText("Upload");
-		button.setOnClickListener(new OnClickListener() {
-			
-			public void onClick(View v) {
-				mBinder.getService().uploadAll("609691583660");
-			}
-		});
-		
-		setContentView(button);
+
+		setContentView(R.layout.activity_upload);
 	}
-	
+
+	private void upload() {
+		mBinder.getService().uploadAll("609691583660");
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getSupportMenuInflater().inflate(R.menu.menu_photo_upload, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+
+		switch (item.getItemId()) {
+			case R.id.menu_upload:
+				upload();
+				return true;
+		}
+
+		return super.onOptionsItemSelected(item);
+	}
+
 	@Override
 	protected void onDestroy() {
 		super.onDestroy();
