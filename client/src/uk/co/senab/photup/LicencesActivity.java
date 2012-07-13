f4947b1c56224a8933f8f8cd0f73bcef9661f00b
Chris Banes <chris@senab.co.uk>
2012-07-13 12:41:29 +0100

    Add Home as Up to Libraries Activity

--- client/src/uk/co/senab/photup/LicencesActivity.java
+++ client/src/uk/co/senab/photup/LicencesActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.webkit.WebView;
 
 import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.MenuItem;
 
 public class LicencesActivity extends SherlockActivity {
 
@@ -11,10 +12,23 @@ public class LicencesActivity extends SherlockActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
 		WebView wv = new WebView(this);
 		setContentView(wv);
 
 		wv.loadUrl("file:///android_asset/libraries.html");
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case android.R.id.home:
+				finish();
+				break;
+		}
+
+		return super.onOptionsItemSelected(item);
 	}
 
 }
