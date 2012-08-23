67cfafa0e4497d8803eea94f06a6075ac946a5d4
Chris Banes <chris@senab.co.uk>
2012-08-23 16:53:42 +0100

    Add some debug to ConnectivityReciever

--- client/src/uk/co/senab/photup/receivers/ConnectivityReceiver.java
+++ client/src/uk/co/senab/photup/receivers/ConnectivityReceiver.java
@@ -1,5 +1,6 @@
 package uk.co.senab.photup.receivers;
 
+import uk.co.senab.photup.Constants;
 import uk.co.senab.photup.PhotoUploadController;
 import uk.co.senab.photup.util.Utils;
 import android.content.BroadcastReceiver;
@@ -7,14 +8,24 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.util.Log;
 
 public class ConnectivityReceiver extends BroadcastReceiver {
+
+	static final String LOG_TAG = "ConnectivityReceiver";
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		if (isConnected(context)) {
+			if (Constants.DEBUG) {
+				Log.d(LOG_TAG, "onReceive - We're connected!");
+			}
+
 			PhotoUploadController controller = PhotoUploadController.getFromContext(context);
 			if (controller.hasWaitingUploads()) {
+				if (Constants.DEBUG) {
+					Log.d(LOG_TAG, "onReceive - Have waiting uploads, starting service!");
+				}
 				context.startService(Utils.getUploadAllIntent(context));
 			}
 		}
