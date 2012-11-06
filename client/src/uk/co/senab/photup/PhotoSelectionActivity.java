60e1ea7d85a7a6545d153187c2e08103f20605db
Chris Banes <chris@senab.co.uk>
2012-11-06 16:09:20 +0000

    Make AB Progress Bar bigger on xlarge tabs

--- client/src/uk/co/senab/photup/PhotoSelectionActivity.java
+++ client/src/uk/co/senab/photup/PhotoSelectionActivity.java
@@ -17,7 +17,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Toast;
