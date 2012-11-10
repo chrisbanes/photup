2235bfa5b204efdca6d9c7fbe31eadeb34af3080
Chris Banes <chris@senab.co.uk>
2012-11-10 18:32:05 +0000

    Remove Flurry

--- client/src/uk/co/senab/photup/util/Analytics.java
+++ client/src/uk/co/senab/photup/util/Analytics.java
@@ -1,14 +1,9 @@
 package uk.co.senab.photup.util;
 
-import uk.co.senab.photup.Flags;
 import android.content.Context;
-
-import com.flurry.android.FlurryAgent;
 
 public class Analytics {
 
-	static final String FLURRY_API_KEY = "6RCVY5ZZ347B6R99QM7J";
-	
 	public static final String EVENT_PHOTO_RESET = "photo_viewer_reset";
 	public static final String EVENT_PHOTO_FILTERS = "photo_viewer_filters";
 	public static final String EVENT_PHOTO_CROP = "photo_viewer_crop";
@@ -16,22 +11,16 @@ public class Analytics {
 	public static final String EVENT_PHOTO_CAPTION = "photo_viewer_caption";
 	public static final String EVENT_PHOTO_PLACE = "photo_viewer_place";
 
-	static {
-		FlurryAgent.setLogEnabled(Flags.DEBUG);
-		FlurryAgent.setCaptureUncaughtExceptions(false);
-		FlurryAgent.setReportLocation(false);
-	}
-
 	public static void onStartSession(Context context) {
-		FlurryAgent.onStartSession(context, FLURRY_API_KEY);
+		//
 	}
 
 	public static void onEndSession(Context context) {
-		FlurryAgent.onEndSession(context);
+		//
 	}
-	
+
 	public static void logEvent(final String event) {
-		FlurryAgent.logEvent(event);
+		//
 	}
 
 }
